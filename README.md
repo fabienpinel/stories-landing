e0f34ff4054aaa8d505fdad829540919d3e615f4
Fabien Pinel <fabienpinel@yahoo.fr>
2018-03-11 21:20:01 +0100

    logos and adding flexbox grid

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stories-landing
 Landing page du magasin Stories
+
+React Flexbox grid
+https://roylee0704.github.io/react-flexbox-grid/
---
+++ stories-app/src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+class Header extends Component {
+    render() {
+        return (
+            <section className="header">
+                <div>
+                    <img
+                        className="logo"
+                        src="../../../assets/img/LogoHD_Color_without_title.png" />
+                </div>
+                <div>
+                    <img
+                        className="logo title"
+                        src="../../../assets/img/Title.png" />
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Header;

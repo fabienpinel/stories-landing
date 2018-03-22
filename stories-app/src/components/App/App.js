2174f79466cd90535f5b6579f0ed29eb4d5d84a9
Fabien Pinel <fabienpinel@yahoo.fr>
2018-03-22 13:24:44 +0100

    MODAL shop openning

--- stories-app/src/components/App/App.js
+++ stories-app/src/components/App/App.js
@@ -5,12 +5,37 @@ import Shop from '../Shop/Shop';
 import Vespa from '../Vespa/Vespa';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
+import Modal from 'react-responsive-modal';
 import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    //check if localstorage contains the alreadyOpenned value
+    let alreadyOpenned = window.localStorage.getItem('modalAlreadyOpennedOnce');
+    this.state = {
+      open: true,
+    };
+  }
+
+  onOpenModal() {
+    this.setState({ open: true });
+  };
+
+  onCloseModal(){
+    this.setState({ open: false });
+    window.localStorage.setItem('modalAlreadyOpennedOnce', true);
+  };
+
+
   render() {
+    const { open } = this.state;
     return (
       <div className="App">
+        <Modal open={open} onClose={() => this.onCloseModal()} little>
+          <h2 className="center">Ouverture du magasin Samedi 31 Mars !</h2>
+          <img src="/assets/img/storiesbandeauxhorizontal.jpg" />
+        </Modal>
         <Header />
         <Shop />
         <Vespa />
